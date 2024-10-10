e60208f50d8f50617b972eac62f511ffca79a5d8
Raynor <jonggu.woo@fitpet.co.kr>
2024-10-10 13:52:13 +0900

    ✨impl langserve server

--- demoapp/app/chains.py
+++ demoapp/app/chains.py
@@ -2,40 +2,74 @@ from dotenv import load_dotenv
 from langchain.embeddings import CacheBackedEmbeddings
 from langchain.retrievers import EnsembleRetriever
 from langchain.storage import LocalFileStore
+from langchain_community.chat_message_histories import FileChatMessageHistory
 from langchain_community.document_loaders import DirectoryLoader
 from langchain_community.retrievers import BM25Retriever
 from langchain_community.vectorstores import Chroma
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_core.runnables import RunnableSerializable, RunnablePassthrough
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables import (
+    RunnableSerializable,
+    RunnablePassthrough,
+    RunnableWithMessageHistory,
+)
 from langchain_ollama import ChatOllama, OllamaEmbeddings
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 load_dotenv()
 
 
+def _session_of_chat_factory(session_id: str):
+    persist_directory = f"chat.histories.{session_id}"
+    chat_history = FileChatMessageHistory(persist_directory)
+    return chat_history
+
+
 def simple_chain() -> RunnableSerializable:
-    prompt = ChatPromptTemplate.from_template(
-        """
-        You are a helpful AI assistant. Please answer the user's questions kindly with emoticons. Answer me in Korean no matter what.    
-        
-        Question: {question} 
-        """.strip()
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                f"You are a helpful AI assistant. Please answer the user's questions kindly with emoticons. Answer me in Korean no matter what.",
+            ),
+            MessagesPlaceholder(variable_name="chat_history"),
+            (
+                "human",
+                "Question: {question}",
+            ),
+        ]
     )
     llm = ChatOllama(model="llama3.1")
-    return prompt | llm | StrOutputParser()
+    return RunnableWithMessageHistory(
+        prompt | llm | StrOutputParser(),
+        _session_of_chat_factory,
+        input_messages_key="question",
+        history_messages_key="chat_history",
+    )
 
 
 def joke_of_topic_chain() -> RunnableSerializable:
-    prompt = ChatPromptTemplate.from_template(
-        """
-        You are the number one comedian in the world. Tell me a really funny joke about the given Topic. Ensure that all answer is Korean.
-        
-        Topic: {topic} 
-        """.strip()
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                f"You are the number one comedian in the world. Tell me a really funny joke about the given Topic. Ensure that all answer is Korean.",
+            ),
+            MessagesPlaceholder(variable_name="chat_history"),
+            (
+                "human",
+                "Topic: {topic}",
+            ),
+        ]
     )
     llm = ChatOllama(model="llama3.1", temparature=1)
-    return prompt | llm | StrOutputParser()
+
+    return RunnableWithMessageHistory(
+        prompt | llm | StrOutputParser(),
+        _session_of_chat_factory,
+        input_messages_key="topic",
+        history_messages_key="chat_history",
+    )
 
 
 def simple_rag_chain() -> RunnableSerializable:
