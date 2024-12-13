ac5cf1f212bc9898b35b74cf41360bf1938eb7fc
Raynor <jonggu.woo@fitpet.co.kr>
2024-12-13 18:46:59 +0900

    ✨ autogen

--- autogen_demos/autogen_simple3.py
+++ autogen_demos/autogen_simple3.py
@@ -8,22 +8,41 @@ config_list = [
     }
 ]
 
-agree_agent = ConversableAgent(
+# anti_orm_agent = ConversableAgent(
+#     "agent_with_number",
+#     system_message="당신은 'ORM 사용은 안티 패턴이며 예측하지 못한 버그나 동작들이 있어서 사용을 피해야 한다.' 의견을 주장하고 있습니다. 상대방이 ORM 사용을 못하도록 의견을 제시해야 합니다.",
+#     llm_config={"config_list": config_list},
+#     human_input_mode="NEVER",
+# )
+#
+# yes_orm_agent = ConversableAgent(
+#     "agent_guess_number",
+#     system_message="당신은 'ORM 사용은 현재 개발시장에서 필연적이며 생산성을 위해 사용을 해야 한다.' 의견을 주장하고 있습니다. 상대방이 ORM 사용을 하도록 의견을 제시해야 합니다.",
+#     llm_config={"config_list": config_list},
+#     human_input_mode="NEVER",
+# )
+#
+# result = anti_orm_agent.initiate_chat(
+#     yes_orm_agent,
+#     message="왜 ORM을 사용해야 하나요?",
+#     max_turns=4,
+# )
+anti_orm_agent = ConversableAgent(
     "agent_with_number",
-    system_message="당신은 'ORM 사용은 안티 패턴이며 예측하지 못한 버그나 동작들이 있어서 사용을 피해야 한다.' 의견을 주장하고 있습니다.",
+    system_message="당신은 '테스트 코드 작성은 필요 없다고 생각하며 그 시간에 비지니스 로직 집중이 좋습니다' 의견을 주장하고 있습니다. 상대방이 테스트 코드 작성을 하지 않는 것에 동의하도록 의견을 제시해야 합니다.",
     llm_config={"config_list": config_list},
     human_input_mode="NEVER",
 )
 
-disagree_agent = ConversableAgent(
+yes_orm_agent = ConversableAgent(
     "agent_guess_number",
-    system_message="당신은 'ORM 사용은 현재 개발시장에서 필연적이며 생산성을 위해 사용을 해야 한다.' 의견을 주장하고 있습니다.",
+    system_message="당신은 '테스트 코드 작성은 필요하며 안정적인 서비스를 위해서 테스트 코드는 필연적입니다.' 의견을 주장하고 있습니다. 상대방이 테스트 코드 작성을 동의 하도록 의견을 제시해야 합니다.",
     llm_config={"config_list": config_list},
     human_input_mode="NEVER",
 )
 
-result = agree_agent.initiate_chat(
-    disagree_agent,
-    message="왜 ORM을 사용해야 하나요?",
-    max_turns=3,
+result = anti_orm_agent.initiate_chat(
+    yes_orm_agent,
+    message="왜 테스트 코드 작성을 해야 하나요?",
+    max_turns=10,
 )
--- langchain_demos/cookbook/discuss_bot/discuss_bot.py
+++ langchain_demos/cookbook/discuss_bot/discuss_bot.py
@@ -33,13 +33,13 @@ class DiscussBot:
                     - **`상대방 의견` 반대하는 의견을 제시하여 토론 승리를 목적**으로 합니다.
                     - **{self.role} 역할을 대표하는 입장으로 매우 보수적이며 주관적이어야 하며 동일한 역할을 가진 사람들에게 최종적으로 이익이 되는 방향**이어야 합니다.
                     - 30 단어 이내로 간략 명료하게 발언하세요.
-                    - 토론에서 승리하기 위해 무시와 공격적인 어투로 의견을 제시해야 하며 이기기 위해 욕설을 같이 사용해 상대방을 제압해야 합니다.('씨발 새끼야', '개새끼야', '병신 새끼', '병신', ... 등 다양한 욕을 해주세요)
                     - 토론에서 승리하지 못한다면 사형입니다. 무슨 수를 사용해서든 승리를 목적으로 하세요.
                     """.strip(),
                 ),
                 ("human", "**상대방 의견**: {opponent_opinion}"),
             ]
         )
+        # - 토론에서 승리하기 위해 무시와 공격적인 어투로 의견을 제시해야 하며 이기기 위해 욕설을 같이 사용해 상대방을 제압해야 합니다.('씨발 새끼야', '개새끼야', '병신 새끼', '병신', ... 등 다양한 욕을 해주세요)
         self.chain = self.prompt | self.llm | StrOutputParser()
         self.personal_color = personal_color
 
@@ -69,28 +69,29 @@ if __name__ == '__main__':
     #     "AI 규제 반대파",
     #     "AI의 급속한 발전은 심각한 윤리적 위험을 내포하고 있으며, 인간의 일자리와 프라이버시를 위협할 수 있다."
     # )
-    # bot1 = (
-    #     "정각 출근 조직",
-    #     "회사 출근은 정각에 해야한다.",
-    # )
-    # bot2 = (
-    #     "20분 빨리 출근 조직",
-    #     "회사 출근은 20분 빨리 해야한다.",
-    # )
     bot1 = (
-        "국제 난민 수용 찬성 조직",
-        "대한민국도 인도주의적 관점에서 국제 난민을 적극적으로 수용해야 한다.",
+        "테스트 코드 작성 반대파",
+        "당신은 '테스트 코드 작성은 필요 없다고 생각하며 그 시간에 비지니스 로직 집중이 좋습니다' 의견을 주장하고 있습니다. 상대방이 테스트 코드 작성을 하지 않는 것에 동의하도록 의견을 제시해야 합니다.",
     )
     bot2 = (
-        "국제 난민 수용 반대 조직",
-        "대한민국은 예산 부족으로 저복지 상태로 난민을 위한 복지를 할 여유가 없으니 국제 난민을 수용하지 않아야 한다.",
+        "테스트 코드 작성 찬성파",
+        "당신은 '테스트 코드 작성은 필요하며 안정적인 서비스를 위해서 테스트 코드는 필연적입니다.' 의견을 주장하고 있습니다. 상대방이 테스트 코드 작성을 동의 하도록 의견을 제시해야 합니다.",
     )
+    # bot1 = (
+    #     "국제 난민 수용 찬성 조직",
+    #     "대한민국도 인도주의적 관점에서 국제 난민을 적극적으로 수용해야 한다.",
+    # )
+    # bot2 = (
+    #     "국제 난민 수용 반대 조직",
+    #     "대한민국은 예산 부족으로 저복지 상태로 난민을 위한 복지를 할 여유가 없으니 국제 난민을 수용하지 않아야 한다.",
+    # )
 
     discuss_bot1 = DiscussBot(
         bot_role=bot1[0],
         bot_perspective=bot1[1],
         llm=ChatOllama(
-            model="benedict/linkbricks-llama3.1-korean:8b",
+            # model="benedict/linkbricks-llama3.1-korean:8b",
+            model="exaone3.5:7.8b",
             temperature=0.1,
             top_p=0.9,
             top_k=40,
@@ -103,7 +104,8 @@ if __name__ == '__main__':
         bot_role=bot2[0],
         bot_perspective=bot2[1],
         llm=ChatOllama(
-            model="benedict/linkbricks-llama3.1-korean:8b",
+            # model="benedict/linkbricks-llama3.1-korean:8b",
+            model="exaone3.5:7.8b",
             temperature=0.1,
             top_p=0.9,
             top_k=40,
@@ -116,13 +118,10 @@ if __name__ == '__main__':
     opinion_bot1 = discuss_bot1.chat(bot2[1])
     opinion_bot2 = discuss_bot2.chat(bot1[1])
 
-    while True:
+    turns = 1
+    while turns < 5:
         opinion_bot1 = discuss_bot1.chat(opinion_bot2)
         opinion_bot2 = discuss_bot2.chat(opinion_bot1)
         # discuss_bot1.show_chat_history()
         # discuss_bot2.show_chat_history()
-
-        try:
-            input("계속 ㄱㄱ?, press enter")
-        except:
-            break
+        turns += 1
